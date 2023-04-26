89fab3ee33c6f4f7de882d2d12335228f143a605
Ubuntu <ubuntu@ip-172-31-10-46.ec2.internal>
2023-04-26 00:49:25 +0000

    script for linux

--- IFC Conversion/ICC_to_GLB.bat
+++ IFC Conversion/ICC_to_GLB.bat
@@ -1,1 +1,3 @@
 forfiles /P "C:\Users\Administrator\Documents\LeanPlanner\IFC Conversion" /M *.ifc /C "cmd /c IfcConvert --use-element-guids --strict-tolerance -j 4 @file @fname.glb"
+
+# find /home/ubuntu/Lean/LeanPlanner/IFC Conversion/ -name "*.ifc" -exec echo {}
