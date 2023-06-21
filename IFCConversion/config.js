af5ce6cd3e49e62da7e5670fd24bd29f30364c3f
Derek <deejema@gmail.com>
2023-06-20 17:04:11 -0700

    config

--- IFCConversion/config.js
+++ IFCConversion/config.js
@@ -2,8 +2,8 @@ const config = {
     port: 3000,
     client_id: '8DZOpGjv0gPk24qAKvTXDlAIA9kDtlKA',
     client_secret: '33qbuejKqphoixHp',
-    awsAccessKey: 'AKIAR5GV47IV5PCQ4QW5',
-    awsSecretKey: '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',
+    awsAccessKey: 'AKIAR5GV47IV2FMZF5LY',
+    awsSecretKey: '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',
     awsBucket: 'leanplannerlinux'
 }
 
