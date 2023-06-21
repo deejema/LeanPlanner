454c41ffa54274096e6f827c0d4a2d6551e9a930
Derek <deejema@gmail.com>
2023-06-20 17:03:19 -0700

    config

--- Login/config.js
+++ Login/config.js
@@ -4,8 +4,8 @@ const config = {
     maxProjectPort:3005,
     urlFiles: 'ec2instances.csv',
     urlBase: 'ec2-44-203-212-239.compute-1.amazonaws.com',
-    awsAccessKey: 'AKIAR5GV47IV5PCQ4QW5',
-    awsSecretKey: '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',
+    awsAccessKey: 'AKIAR5GV47IV2FMZF5LY',
+    awsSecretKey: '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',
     awsBucket: 'leanplannerlinux'
 }
 
