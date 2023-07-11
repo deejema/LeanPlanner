85d2633cdcb826f401da0b61bf089099ae5de41b
Derek <deejema@gmail.com>
2023-07-11 15:55:36 -0700

    delete all files

--- IFCConversion/index.js
+++ IFCConversion/index.js
@@ -348,13 +348,13 @@ function uploadToS3() {
     console.log('DO PYTHON SCRIPT HERE')
     let pythonCmd = `python3 Parsing_XML_Data.py ${pythonScriptFiles[0]} ${pythonScriptFiles[1]}` // 0 = xml, 1 = json
     console.log('PYTHON COMMAND: ', pythonCmd)
-    callCmd(pythonCmd, removeAllFiles);
+    callCmd(pythonCmd);
 
     // REMOVE ALL FILES (GLB, Config Files, XML, JSON)
-    // removeAllFiles();
+    removeAllFiles();
 
 }
-function callCmd(cmdline, callback) {
+function callCmd(cmdline) {
     let cmd = spawn (cmdline, [], {shell:true});
     cmd.stdout.on("data", data => {
         console.log(`stdout: ${data}`);
@@ -369,7 +369,6 @@ function callCmd(cmdline, callback) {
     });
     cmd.on("close", code => {
         console.log(`child process exited with code ${code}`);
-        callback;
     })
 }
 function removeAllFiles() {
