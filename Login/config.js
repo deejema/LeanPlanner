45e1dd8a6384b7eef90953f276ab32ec3498a2be
Derek <deejema@gmail.com>
2023-09-12 15:52:51 -0700

    Changed config.js for login

--- Login/config.js
+++ Login/config.js
@@ -3,7 +3,7 @@ const config = {
     firstProjectPort:3001,
     maxProjectPort:3005,
     urlFiles: 'ec2instances.csv',
-    urlBase: 'ec2-44-203-212-239.compute-1.amazonaws.com',
+    urlBase: 'ec2-3-235-92-162.compute-1.amazonaws.com',
     awsAccessKey: 'AKIAR5GV47IV2FMZF5LY',
     awsSecretKey: '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',
     awsBucket: 'leanplannerlinux'
