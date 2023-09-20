5c3af36a2ad8dfc60b582175e254427d335584aa
Derek <deejema@gmail.com>
2023-09-19 23:32:18 -0700

    slee

--- IFCConversion/index.js
+++ IFCConversion/index.js
@@ -448,7 +448,7 @@ async function uploadToS3() {
 
     callCmd(pythonCmd, true);
 
-    await sleep(5000);
+    await sleep(20000);
     // REMOVE ALL FILES (GLB, Config Files, XML, JSON)
     removeAllFiles();
 }
