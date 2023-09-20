72256217629cbe3bb9f1755be610f53e2e8ccac3
Derek <deejema@gmail.com>
2023-09-20 01:12:36 -0700

    fdsafsda

--- Login/services/projects.js
+++ Login/services/projects.js
@@ -41,7 +41,7 @@ class ProjectGenerationService {
                     cp -r /home/ubuntu//Lean/ForgeTemplate/ ${projectDirectory}
                     mv /home/ubuntu/.env ${projectDirectory} 
                     mv /home/ubuntu/urns.js ${projectDirectory}wwwroot
-                    cd ${projectDirectory}
+                    cd
                     ./.nvm/versions/node/v14.16.0/bin/pm2 start server.js`;
                     fs.writeFileSync('scriptToForge', script);
                     return port;
