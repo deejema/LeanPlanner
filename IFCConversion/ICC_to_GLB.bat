4a6edbafc410fb192ce8805536a2b2997f0aebfc
Derek <deejema@gmail.com>
2023-10-06 15:38:33 -0700

    bat change, ignore

--- IFCConversion/ICC_to_GLB.bat
+++ IFCConversion/ICC_to_GLB.bat
@@ -1,3 +1,4 @@
 forfiles /P "C:\Users\Administrator\Documents\LeanPlanner\IFC Conversion" /M *.ifc /C "cmd /c IfcConvert --use-element-guids --strict-tolerance -j 4 @file @fname.glb"
+forfiles /P "C:\Users\Administrator\Documents\LeanPlanner\IFC Conversion" /M *.ifc /C "cmd /c IfcConvert --use-element-guids --strict-tolerance -j 4 @file @fname.xml"
 
 # find /home/ubuntu/Lean/LeanPlanner/IFC Conversion/ -name "*.ifc" -exec echo {}
